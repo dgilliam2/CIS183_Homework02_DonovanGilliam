a4bbf8635d71e3341e02261aa027b18c6fcc2050
dgilliam2 <165728152+dgilliam2@users.noreply.github.com>
2024-09-23 14:30:08 -0400

    init

--- app/src/main/java/com/example/cis183_homework02_donovangilliam/MainActivity.java
+++ app/src/main/java/com/example/cis183_homework02_donovangilliam/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.cis183_homework02_donovangilliam;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,13 +17,17 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
+    TextView tv_j_red;
+    TextView tv_j_green;
+    TextView tv_j_blue;
+
     Button btn_j_savecolor;
     ListView lv_j_storedcolors;
     SeekBar sb_j_red;
     SeekBar sb_j_green;
     SeekBar sb_j_blue;
 
-    ArrayList<ColorInfo> stored_colors;
+    ArrayList<ColorInfo> stored_colors = new ArrayList<ColorInfo>();
 
     StoredColorAdapter adapter;
 
@@ -32,13 +38,85 @@ public class MainActivity extends AppCompatActivity
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+
+        tv_j_red = findViewById(R.id.tv_v_red);
+        tv_j_green = findViewById(R.id.tv_v_green);
+        tv_j_blue = findViewById(R.id.tv_v_blue);
+
         btn_j_savecolor = findViewById(R.id.btn_v_savecolor);
         lv_j_storedcolors = findViewById(R.id.lv_v_storedcolors);
         sb_j_red = findViewById(R.id.sb_v_red);
         sb_j_green = findViewById(R.id.sb_v_green);
         sb_j_blue = findViewById(R.id.sb_v_blue);
+
+        seekBarListener();
     }
 
+    private void seekBarListener()
+    {
+        sb_j_red.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                tv_j_red.setText("Red: " + String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
+
+        sb_j_green.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                tv_j_green.setText("Green: " + String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
+
+        sb_j_blue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                tv_j_blue.setText("Blue: " + String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
+    }
 
 
 }
